f569be3f00d3657811cdfac86547b4128a44e182
ahernot <70817470+ahernot@users.noreply.github.com>
2020-11-16 12:58:11 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,5 +1,7 @@
 
 #   IMPORTING THIRD-PARTY LIBRARIES
+#import os
+import numpy
 import cv2
 
 #   IMPORTING PROJECT FILES
@@ -8,3 +10,17 @@ import cv2
 #   DEFINING FILE-WIDE VARIABLES
 # -
 
+
+
+class Image(object):
+
+    def __init__(self, image_array: numpy.ndarray):
+
+        self.array = image_array
+
+
+
+
+    def add(self, image: Image):
+        # will concatenate another IMAGE
+        pass
