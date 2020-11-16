f46e03b03bfe3b69a4e7b188a60334c130d6dc55
ahernot <70817470+ahernot@users.noreply.github.com>
2020-11-16 13:07:50 +0100

    Create directory_reading.py

---
+++ directory_reading.py
@@ -0,0 +1,14 @@
+
+#   IMPORTING THIRD-PARTY LIBRARIES
+#import os
+import numpy
+import cv2
+
+#   IMPORTING PROJECT FILES
+# -
+
+#   DEFINING FILE-WIDE VARIABLES
+# -
+
+
+# will run through the input directory and import all the images (using OpenCV cv2)
