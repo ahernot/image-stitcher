83e16cc2233845cd55988b564c574df423e77eb1
ahernot <70817470+ahernot@users.noreply.github.com>
2020-11-16 12:54:37 +0100

    Create __main___.py

---
+++ __main___.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#   IMPORTING THIRD-PARTY LIBRARIES
+# -
+
+#   IMPORTING PROJECT FILES
+# -
+
+#   DEFINING FILE-WIDE VARIABLES
+# -
+
+
+
+
+if __name__ == '__main__':
+    pass
